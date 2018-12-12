41a9f52c413f73887c16634afed87e6e8822e622
robert <robert@redcor.ch>
2018-12-12 21:07:34 +0100

    FIX: changed docker creation template

--- templates/docker_templates.py
+++ templates/docker_templates.py
@@ -72,10 +72,27 @@ docker_base_file_template = """
 FROM %(erp_image_version)s
 MAINTAINER robert@redo2oo.ch
 
+# For installing odoo you have two possibility
+# 1. either adding the whole root directory
+#COPY . /odoo
+
+# 2. or adding each directory, this solution will reduce the build and download
+# time of the image on the server (layers are reused)
+COPY ./src /odoo/src
+COPY ./external-src /odoo/external-src
+COPY ./local-src /odoo/local-src
+COPY ./data /odoo/data
+COPY ./songs /odoo/songs
+COPY ./setup.py /odoo/
+COPY ./VERSION /odoo/
+COPY ./migration.yml /odoo/
+RUN pip install --cache-dir=.pip -e /odoo
+RUN pip install --cache-dir=.pip -e /odoo/src
+
 %(run_block)s
 
 COPY ./requirements.txt /opt/odoo/
-RUN cd /opt/odoo && pip install -r requirements.txt
+RUN cd /opt/odoo && pip install --cache-dir=.pip -r requirements.txt
 
 ENV ADDONS_PATH=/opt/odoo/local-src,/opt/odoo/src/addons
 #ENV DB_NAME=afbsdemo
