9d181712f2ab418f3df21ced9c8db16389d0c9c5
Robert Rottermann <robert@redcor.ch>
2019-01-01 22:56:07 +0100

    added test sites_pw.py

---
+++ tests/sites_pw.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# global password: RedcorKmuOnFrieda$99
+
+# Your personal key: ed9a644a89302a5367285c490c36d05f
+# You can check the use of your key here: http://tel.search.ch/api/help.en.html
+
+# Lisa : lwzVoEqsfb
+SITES_PW = {
+    'docker_hub' : {
+        'docker_hub' : {
+            'testaccount' : {'docker_hub_pw' : 'test$pw'},
+        },
+    },
+    "demo_global" : {
+        'odoo_admin_pw': 'demo_global$odoo_admin_pw',
+    },
+}
