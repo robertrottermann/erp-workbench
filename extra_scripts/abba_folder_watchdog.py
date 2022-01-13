528c9600fddcca0da39dd91e589216f3c121132c
robertrottermann <robert@redcor.ch>
2022-01-13 18:05:31 +0100

    [WIP]

--- extra_scripts/abba_folder_watchdog.py
+++ extra_scripts/abba_folder_watchdog.py
@@ -24,24 +24,59 @@
 #     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
 
-import sys
 import time
-import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+class Watcher:
+
+    def on_created(self, event):
+        print(f"hey, {event.src_path} has been created!")
+    
+    def on_deleted(self, event):
+        print(f"what the f**k! Someone deleted {event.src_path}!")
+    
+    def on_modified(self, event):
+        print(f"hey buddy, {event.src_path} has been modified")
+    
+    def on_moved(self, event):
+        print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+        
+    def __init__(self, directory=".", handler=FileSystemEventHandler()):
+        self.observer = Observer()
+        self.handler = handler
+        self.directory = directory
+        
+        # monkey patch handler
+        self.handler.on_created = self.on_created
+        self.handler.on_deleted = self.on_deleted
+        self.handler.on_modified = self.on_modified
+        self.handler.on_moved = self.on_moved
+        
+
+    def run(self):
+        self.observer.schedule(
+            self.handler, self.directory, recursive=True)
+        self.observer.start()
+        print("\nWatcher Running in {}/\n".format(self.directory))
+        try:
+            while True:
+                time.sleep(1)
+        except:
+            self.observer.stop()
+        self.observer.join() # Wait until the thread terminates.
+        print("\nWatcher Terminated\n")
+
+
+class MyHandler(FileSystemEventHandler):
+
+    def on_any_event(self, event):
+        if event.event_type == "deleted":
+                    print("Oh no! It's gone!")
+        if event.src_path[-5:] == ".xml":
+                    print("Microsoft Word documents not supported.")                
+        print(event) # Your code here
+
+if __name__=="__main__":
+    w = Watcher(".", MyHandler())
+    w.run()
